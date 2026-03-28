25f403e2c1554dab90121d4b317f262c6e50a040
dcarrier <carrierdarius@gmail.com>
2026-03-28 14:00:48 -0700

    add boardsource/unicorne

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "QMK firmware development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "qmk-dev";
+
+        packages = with pkgs; [
+          qmk
+        ];
+      };
+    });
+}
